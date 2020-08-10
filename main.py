952c44e53b6dfaf1570cd53e1a1eb4e819859f15
Nathan <nathanrjulian@gmail.com>
2020-08-10 17:42:24 +0000

    finished intro num. 6

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#1
 name = input('what is your name?  ')
 print('Hello ' + name)
-
+#2
 x = 10
 print(x)
-
+#3
 number = input('Enter number:  ')
 num = int(number)
 
@@ -13,7 +14,7 @@ elif(num > 10):
 	print('number is greater than 10')
 elif(num < 10):
 	print('number is less than 10')
-
+#4
 number2 = input('Enter a number:   ')
 num2 = int(number2)
 
@@ -21,8 +22,13 @@ if (num2 % 2 == 0):
 	print('the number is even')
 else:
 	print('the number is odd')
-
+#5
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for x in list:
 	print(list[(x - 1)])
+#6
+list2 = []
+for i in range(10):
+	list2.append(i + 1)
+print(list2)
