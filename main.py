2fa38c0cbb0832a0a4a6be838201c7ebeacba3fe
Nathan <nathanrjulian@gmail.com>
2020-08-11 17:26:04 +0000

    finished. a bug at end, but its fine

--- main.py
+++ main.py
@@ -26,10 +26,41 @@ else:
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for x in list:
-	print(list[(x - 1)])
+	print(x)
 #6
 list2 = []
 for i in range(10):
 	list2.append(i + 1)
 print(list2)
 #7
+
+lessThanList = [1, 2, 44, 3467, 87, 65, 47, 5467, 34534534, 4, -1, -438, -2934, -7, -5]
+lessThanList.sort()
+GreaterNum = input('Enter number:  ')
+greaterNum = int(GreaterNum)
+amountGreater = 0
+
+for i in range(len(lessThanList)):
+	if (greaterNum > lessThanList[i]):
+		amountGreater += 1
+
+amountGreater = str(amountGreater)
+print( 'Your number has '+ amountGreater + ' numbers in list less than it')
+
+#8
+while True:
+	word = input("word: ")
+	if word == "stop":
+		break
+	for letter in word:
+		print(letter)
+#9
+
+avgList = []
+while True:
+	numNum = input('number:  ')
+	if (numNum == "stop"):
+		avg = (sum(avgList) / len(avgList))
+		print(f"The mean is {avg}")
+	numNum = float(numNum)
+	avgList.append(numNum)
